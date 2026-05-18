fc662bdfb02257ae111566d63d3445cb1f4b6bf4
Tal <talrofeh@gmail.com>
2026-05-18 12:36:36 +0300

    change to update on the fly

---
+++ AdvancedVehicleOptions/Patches/RaceCarAIPatch.cs
@@ -0,0 +1,23 @@
+﻿using ColossalFramework;
+using HarmonyLib;
+
+namespace AdvancedVehicleOptionsUID.Patches
+{
+    [HarmonyPatch]
+    public static class RaceCarAIPatch
+    {
+        [HarmonyPatch(typeof(RaceCarAI), "SimulationStep", [typeof(ushort), typeof(Vehicle), typeof(Vehicle.Frame), typeof(ushort), typeof(Vehicle), typeof(int)], 
+            [ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal])]
+        [HarmonyPrefix]
+        public static void SimulationStep(ushort vehicleID, ref Vehicle vehicleData, ref Vehicle.Frame frameData, ushort leaderID, ref Vehicle leaderData, int lodPhysics)
+        {
+            Building building = Singleton<BuildingManager>.instance.m_buildings.m_buffer[vehicleData.m_sourceBuilding];
+            EventData eventData = Singleton<EventManager>.instance.m_events.m_buffer[building.m_eventIndex];
+            ref RacerData racerData = ref eventData.m_raceEventData.m_racerData[vehicleData.m_racerIndex];
+            racerData.m_maxSpeed = vehicleData.Info.m_maxSpeed;
+            racerData.m_acceleration = vehicleData.Info.m_acceleration;
+            racerData.m_braking = vehicleData.Info.m_braking;
+            racerData.m_turning = vehicleData.Info.m_turning;
+        }
+    }
+}
---
+++ AdvancedVehicleOptions/Patches/RaceCitizenAIPatch.cs
@@ -0,0 +1,32 @@
+﻿using ColossalFramework;
+using HarmonyLib;
+
+namespace AdvancedVehicleOptionsUID.Patches
+{
+    [HarmonyPatch]
+    public static class RaceCitizenAIPatch
+    {
+        [HarmonyPatch(typeof(RaceCitizenAI), "SimulationStep")]
+        [HarmonyPrefix]
+        public static void SimulationStep(ushort instanceID, ref CitizenInstance citizenData, ref CitizenInstance.Frame frameData, bool lodPhysics)
+        {
+            Building building = Singleton<BuildingManager>.instance.m_buildings.m_buffer[citizenData.m_sourceBuilding];
+            EventData eventData = Singleton<EventManager>.instance.m_events.m_buffer[building.m_eventIndex];
+
+            if(eventData.m_raceEventData == null) return;
+
+            var citizen = Singleton<CitizenManager>.instance.m_citizens.m_buffer[citizenData.m_citizen];
+            if (citizen.m_vehicle == 0) return;
+
+            Vehicle race_vehicle = Singleton<VehicleManager>.instance.m_vehicles.m_buffer[citizen.m_vehicle];
+
+            if (race_vehicle.Info == null) return;
+
+            if ((race_vehicle.Info.vehicleCategory & VehicleInfo.VehicleCategory.Bicycle) == 0) return;
+
+            ref RacerData racerData = ref eventData.m_raceEventData.m_racerData[citizenData.m_racerIndex];
+            racerData.m_maxSpeed = race_vehicle.Info.m_maxSpeed;
+            racerData.m_acceleration = race_vehicle.Info.m_acceleration;
+        }
+    }
+}
